a64fb2ab2c28dfb41ce5ecbc8f4841ef7fca0d8f
Johannes <johannes.barjak@gmail.com>
2024-06-05 12:06:33 -0500

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "A flake for game of life";
+
+  outputs = { self, nixpkgs ? import <nixpkgs> }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+    pkgName = "gameOfLife";
+    haskPkgs = pkgs.haskell.packages.ghc965;
+
+  in {
+    packages.${system}.${pkgName} = haskPkgs.developPackage { root  = ./.; };
+    defaultPackage.${system} = self.packages.${system}.${pkgName};
+
+    devShells = {
+      ${system}.default = pkgs.mkShell {
+        buildInputs = [
+          ( haskPkgs.ghcWithPackages ( p: [
+            p.haskell-language-server
+            p.ghcid
+            p.cabal-install
+          ]))
+        ];
+      };
+    };
+  };
+}
