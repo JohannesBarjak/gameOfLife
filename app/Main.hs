0405cd7c009d6b1d1bddb804fa1908842f4634cc
Johannes <johannes.barjak@gmail.com>
2024-06-05 12:59:10 -0500

    Add yampa dependecy and rename library module

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,8 @@
 module Main where
 
-import qualified MyLib (someFunc)
+import qualified GameOfLife (someFunc)
 
 main :: IO ()
 main = do
   putStrLn "Hello, Haskell!"
-  MyLib.someFunc
+  GameOfLife.someFunc
